479c6660a2c4db2f35a2bbed2f94716e6386ffdf
Brayan Almengor <brayanalmengor300@gmail.com>
2025-05-21 14:38:10 -0500

    feature : create endpoint of category backend

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -1,23 +1,19 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace App\Http\Controllers\Api;
+use App\Http\Controllers\Controller; 
 use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+    public function index(){
+    try {$categories = Category::all();
+        return response()->json([$categories], 200);
+    } catch (\Exception $e) {return response()->json([$e->getMessage()
+        ], 500);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
+    }
     public function create()
     {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\public\PublicApiController;
+use App\Http\Controllers\Api\CategoryController;
 
+// Test Api 
 Route::get('/', function () {
-    return view('welcome');
-});
-// Prueba que todo okey... 
-Route::get('/public', [PublicApiController::class, 'test']);
-Route::get('/categories', [PublicApiController::class, 'categories']);
+    return response()->json(['message' => 'API funcionando']);
+});  
 
+//https://saborespty/api/v1/categories 
+// Grupo de rutas con prefijo 'api/v1' para versión de la API
+Route::prefix('api/v1')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+});
